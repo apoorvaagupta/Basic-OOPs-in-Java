9647be694d58a681ffbdd13742191dc5acce16b2
Apoorvaa Gupta <apoorvaagupta01@gmail.com>
2018-03-23 21:37:26 +0530

    Processor class that adds any two vars
    Signed-off-by: Apoorvaa Gupta <apoorvaagupta01@gmail.com>

--- src/main/java/me/apoorvaagupta/Processor.java
+++ src/main/java/me/apoorvaagupta/Processor.java
@@ -1,4 +1,28 @@
 package me.apoorvaagupta;
 
-public class Processor {
+import com.sun.jdi.IntegerValue;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Processor<T> {
+
+    private final Class<T> classname;
+
+    public Processor(Class<T> classanme) {
+        this.classname = classanme;
+    }
+
+    public <T> T process(T var1, T var2) {
+
+        if (this.classname == Integer.class) {
+            return (T) new Integer((Integer) var1 +  (Integer) var2);
+        } else {
+            return (T) ((String) var1 + (String) var2);
+        }
+
+
+    }
+
+
 }
